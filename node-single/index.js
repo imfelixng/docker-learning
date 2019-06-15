dec109f063060f257bd0f3d7377aae54e444396b
Nguyen Quang An <ngquangan@gmail.com>
2019-06-16 03:23:59 +0700

    Init node project

---
+++ node-single/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello');
+})
+
+app.listen(8080, () => console.log('Server is running at port 8080'))
