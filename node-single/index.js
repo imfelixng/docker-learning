3dac7795c2800a9bc09ff89768e08de5177e5379
Nguyen Quang An <ngquangan@gmail.com>
2019-06-16 12:23:00 +0700

    Success create dockerfile for node app

--- node-single/index.js
+++ node-single/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello');
+  res.send('Hello abc');
 })
 
 app.listen(8080, () => console.log('Server is running at port 8080'))
