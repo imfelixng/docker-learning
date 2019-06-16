0571c071d2952941de96b211f41d1a7348bac764
Nguyen Quang An <ngquangan@gmail.com>
2019-06-16 12:47:51 +0700

    Init project and setup express and redis

---
+++ node-multi/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const redis = require('redis');
+
+const app = express();
+
+const client = redis.createClient();
+client.set('visits', 0);
+
+app.get('/', (req, res) => {
+  client.get('visits', (err, visits) => {
+    res.send('Number of visit is ' + visits)
+    client.set('visits', visits + 1);
+  });
+});
+
+app.listen(8081, () => console.log('Server is running at ' + 8081))
