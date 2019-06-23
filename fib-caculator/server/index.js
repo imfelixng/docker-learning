19f0c82a786415da0468ab6cf09eb1a95dcd5af4
Nguyen Quang An <ngquangan@gmail.com>
2019-06-23 12:27:37 +0700

    Finish config server and create client with react

--- fib-caculator/server/index.js
+++ fib-caculator/server/index.js
@@ -33,3 +33,38 @@ const redisClient = redis.createClient({
   port: keys.redisPort,
   retry_strategy: () => 1000
 });
+
+const redisPublisher = redisClient.duplicate();
+
+app.get('/', (req, res) => {
+  res.send('Hi');
+});
+
+app.get('/values/all', async (req, res) => {
+  const values = await pgClient.query('SELECT * FROM values');
+  res.send(values.rows);
+});
+
+app.get('/values/current', async (req, res) => {
+  redisClient.hgetall('values', (err, values) => {
+    res.send(values);
+  })
+});
+
+app.post('/values', async (req, res) => {
+  const index = req.body.index;
+
+  if (+index > 40) {
+    return res.status(422).send('Index too high')
+  }
+
+  redisClient.hset('values', index, 'Nothing yet!');
+  redisPublisher.publish('insert', index);
+  pgClient.query('INSERT INTO values(number) VALUES($1)', [index])
+
+  res.send({ working: true })
+});
+
+app.listen(5000, () => {
+  console.log('Server is running...')
+})
